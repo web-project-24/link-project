0e5e343c6a243527312f697ae46edb224af84009
SeongMinnnn <psm4502@gmail.com>
2023-01-10 18:29:40 +0900

    코드 작성

--- app.py
+++ app.py
@@ -13,31 +13,56 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+
 # 링크 전체 조회
-@app.route("/api/link", methods=["GET"])
+@app.route("/api/link<int:id>", methods=["GET"])
 def link_list_get():
-    _linklist = request.args.get('link')
+    _linklist = list(db.links.find({}, {'_id': False}))
 
     return jsonify({'linklist': _linklist})
+
 
 # 링크 생성
 @app.route("/api/link", methods=["POST"])
 def link_post():
-    _title = request.form['title']
 
+    id = request.form['id']
+    title = request.form['title']
+    url = request.form['url']
+    tag = request.form['tag']
+    author = request.form['author']
+    image = request.form['image']
+
+    doc = {
+        'id': id,
+        'title': title,
+        'url': url,
+        'tag': tag,
+        'author': author,
+        'image': image
+    }
+
+    db.links.insert_one(doc)
     return jsonify({'msg': '링크 등록 완료!'})
 
 # 링크 수정
 @app.route("/api/link/<int:id>", methods=["PUT"])
 def link_put(id):
-    _title = request.form['title']
-
+    db.links.update_one(
+        {'id': id},
+        {'title': title},
+        {'$set': {'url': url_receive}},
+        {'tag': tag},
+        {'author': author},
+        {'image': image}
+    )
     return jsonify({'msg': '링크 수정 완료!', 'path': id})
+
 
 # 링크 삭제
 @app.route("/api/link/<int:id>", methods=["DELETE"])
 def link_delete(id):
-
+    db.links.delete_one({'id': id})
 
     return jsonify({'msg': '링크 삭제 완료!', 'path': id})
 
